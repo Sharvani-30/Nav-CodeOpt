1e6f42d401104d949c3c2e2be47971f3face252b
Sharvani <sharvanikomuravelly2004@gmail.com>
2024-11-18 14:53:53 +0530

    made changes
    responsive footer and header

--- FRONTEND/src/components/Navbar.js
+++ FRONTEND/src/components/Navbar.js
@@ -1,40 +1,45 @@
-import React, {useState} from 'react'
-import Logo from "../assests/logo.png"
-import {Link} from 'react-router-dom'
-import '../styles/navbar.css'
+import React, { useState } from 'react';
+import Logo from "../assests/logo.png";
+import { Link } from 'react-router-dom';
+import '../styles/navbar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
+
 function Navbar() {
-  const [openLinks, setOpenLinks]=useState(false);
+  const [openLinks, setOpenLinks] = useState(false);
+
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
   return (
     <div className="navbar">
-        <div className="leftSide"id={openLinks ?"open":"close"}>
-            <img src={Logo}/>
-            <div className="title">
-          Code Optimizer
-        </div>
-        <div className="hiddenLinks">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-        </div>
-        
-        <div className="rightSide">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/contact">Contact</Link>
-          <button onClick={toggleNavbar}>
-            <ReorderIcon/>
-          </button>
-        </div>
+      <div className="leftSide">
+        <img src={Logo} alt="Logo" />
+        <div className="title">Code Optimizer</div>
+      </div>
 
+      <div className={`rightSide ${openLinks ? "active" : ""}`}>
+        <div className="navLinks">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <button className="toggleButton" onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
+      </div>
+
+      {openLinks && (
+        <div className="menu">
+          <Link to="/" onClick={toggleNavbar}>Home</Link>
+          <Link to="/about" onClick={toggleNavbar}>About</Link>
+          <Link to="/services" onClick={toggleNavbar}>Services</Link>
+          <Link to="/contact" onClick={toggleNavbar}>Contact</Link>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Navbar;
